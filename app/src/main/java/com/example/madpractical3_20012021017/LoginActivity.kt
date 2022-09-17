e8a513833d7d685110c5d200706c157cb2a10c0f
Abhikumar Parate <abhiparate2523@gmail.com>
2022-09-17 19:57:13 +0530

    app working without data passing

--- app/src/main/java/com/example/madpractical3_20012021017/LoginActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012021017/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.example.madpractical3_20012021017
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.core.view.WindowCompat
 import com.example.madpractical3_20012021017.databinding.ActivityLoginBinding
 
@@ -14,5 +17,14 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+    }
+
+    fun launchreistration(view: View){
+        val intent= Intent(this,RegistrationActivity::class.java)
+//        intent.putExtra("mainactivity1",msg)
+        startActivity(intent)
+    }
+    fun launchresultlogin(view: View){
+        Toast.makeText(this,"Welcome", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/madpractical3_20012021017/RegistrationActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012021017/RegistrationActivity.kt
@@ -3,6 +3,7 @@ package com.example.madpractical3_20012021017
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AbsListView
 import android.widget.Button
@@ -13,10 +14,6 @@ import com.example.madpractical3_20012021017.databinding.ActivityRegistrationBin
 
 class RegistrationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegistrationBinding
-    lateinit var email:EditText
-    lateinit var password1:EditText
-    lateinit var password2:EditText
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -27,19 +24,11 @@ class RegistrationActivity : AppCompatActivity() {
 //        setContentView(R.layout.activity_registration)
     }
     fun launchlogin(view: View){
-
-        email =findViewById(R.id.emailonreg)
-        password1 = findViewById(R.id.passwordonreg)
-        password2 = findViewById(R.id.conpasswordonreg)
-        var msg = email.text.toString()
-        Toast.makeText(this,msg, Toast.LENGTH_LONG).show()
-        if (password1 == password2)
-        {
-            val intent= Intent(this,LoginActivity::class.java)
-            startActivity(intent)
-        } else {
-            Toast.makeText(this,"Enter correct password", Toast.LENGTH_LONG).show()
-        }
-
+        val intent= Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+        Toast.makeText(this,"Login button is clicked",Toast.LENGTH_LONG).show()
+    }
+    fun launchresult(view: View){
+        Toast.makeText(this,"Welcome",Toast.LENGTH_LONG).show()
     }
 }
